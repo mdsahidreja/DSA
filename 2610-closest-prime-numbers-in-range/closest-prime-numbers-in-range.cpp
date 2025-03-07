a0939d2836c63b4ca9905747f239e666cd51f298
Md Sahid Reja <mdsahidreja878@gmail.com>
2025-03-07 14:00:27 +0530

    Time: 275 ms (37.78%) | Memory: 23.1 MB (47.78%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     vector<int> closestPrimes(int left, int right) {
+        vector<bool> primes = helper(right);
         vector<int>temp;
-        vector<int>ans(2, -1);
-        for(int i = left; i<=right; i++){
-            if(isPrime(i)){
+
+        for(int i = left; i<= right;i++){
+            if(primes[i]){
                 temp.push_back(i);
             }
-            
-            
-        }
-        if(temp.size() < 2){
-            return {-1, -1};
         }
         int minDiff = INT_MAX;
+        vector<int> result(2, -1);
         for(int i = 1; i<temp.size(); i++){
-            if(minDiff > temp[i] - temp[i-1]){
-                minDiff = temp[i] - temp[i-1];
-                ans[0] = temp[i-1];
-                ans[1] = temp[i];
-            }
-            else if(minDiff == temp[i] - temp[i-1] && temp[i-1] < ans[0]){
-                ans[0] = temp[i-1];
-                ans[1] = temp[i];
+            int diff = temp[i] - temp[i-1];
+            if(diff < minDiff){
+                minDiff = diff;
+                result = {temp[i-1], temp[i]};
             }
         }
-        return ans;
+        return result;
     }
-
 private:
-    bool isPrime(int n){
-        if(n == 1 || n == 0){
-            return false;
-        }
-        for(int i = 2; i*i<=n; i++){
-            if(n % i == 0){
-                return false;
+    vector<bool> helper(int right){
+        vector<bool>primes(right+1, true);
+        primes[0] = false;
+        primes[1] = false;
+
+        for(int i = 2; i<=right; i++){
+            if(primes[i]){
+                for(int j = 2*i; j<=right; j= j + i){
+                    primes[j] = false;
+                }
             }
         }
-        return true;
+        return primes;
     }
 };
