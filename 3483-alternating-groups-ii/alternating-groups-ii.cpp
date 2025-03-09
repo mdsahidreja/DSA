31210cf31e97538647d58fff0d425804a322b012
Md Sahid Reja <mdsahidreja878@gmail.com>
2025-03-09 15:33:32 +0530

    Time: 59 ms (92.68%) | Memory: 108.3 MB (24.48%) - LeetSync

---
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        //update the colors array as it is circular
+        for(int i = 0; i<k-1; i++){
+            colors.push_back(colors[i]);
+        }
+
+
+        int n = colors.size();
+        int i = 0;
+        
+        int answer = 0;
+        for(int j = 1; j<n; j++){
+            
+            if(colors[j] == colors[j-1]){
+                // j is not alternating
+                i = j;
+                j = i;
+                continue;//skip the current window and go to the next window
+            }
+
+            if(j-i+1 == k){
+                answer++;
+                i++;
+            }
+        }
+        return answer;
+    }
+};
